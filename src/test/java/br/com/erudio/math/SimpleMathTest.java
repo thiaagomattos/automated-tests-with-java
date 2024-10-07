d4cb51c03ca0d0485947c1926958bf52f73c4ec5
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-07 15:43:25 -0300

    added math tests

--- src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -9,10 +9,83 @@ class SimpleMathTest {
     @Test
     void testSum() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
         
-        assertEquals(expected, actual, "6.2+2 did not produce 8.2!");
-    }
+        assertEquals(expected, actual, () -> firstNumber + "+" + secondNumber + " did not produce " + expected + "!");
+            assertNotEquals(9.2, actual);
+            assertNotNull(actual);
+      }
+    
 
+    @Test
+    void testSubtraction() {
+    	SimpleMath math2 = new SimpleMath(); {
+    		double firstNumber = 6.2D;
+            double secondNumber = 2D;
+            Double actual = math2.subtraction(firstNumber, secondNumber);
+            double expected = 4.2D;
+            
+            assertEquals(expected, actual, () -> firstNumber + "-" + secondNumber + " did not produce " + expected + "!");
+            assertNotEquals(9.2, actual);
+            assertNotNull(actual);
+    	}
+    }
+    
+    @Test
+    void testMultiplication() {
+    	SimpleMath math3 = new SimpleMath(); {
+    		double firstNumber = 5D;
+    		double secondNumber = 2D;
+    		Double actual = math3.multiplication(firstNumber, secondNumber);
+    		double expected = 10D;
+    		
+    		assertEquals(expected, actual, () -> firstNumber + "*" + secondNumber + " did not produce " + expected + " !");
+    		assertNotEquals(9.2D, actual);
+            assertNotNull(actual);
+    	}
+    }
+    
+    @Test
+    void testDivision() {
+    	SimpleMath math4 = new SimpleMath(); {
+    		double firstNumber = 10D;
+    		double secondNumber = 2D;
+    		Double actual = math4.division(firstNumber, secondNumber);
+    		double expected = 5D;
+    		
+    		assertEquals(expected, actual, () -> firstNumber + ":" + secondNumber + " did not produce " + expected + " !");
+    		assertNotEquals(9.2D, actual);
+            assertNotNull(actual);
+    	}
+    }
+    
+    @Test
+    void testMean() {
+    	SimpleMath math5 = new SimpleMath(); {
+    		double firstNumber = 10D;
+    		double secondNumber = 2D;
+    		Double actual = math5.mean(firstNumber, secondNumber);
+    		double expected = 6D;
+    		
+    		assertEquals(expected, actual, () -> "(" + firstNumber + "+" + secondNumber + ")" + " /2 " + " did not produce " + expected + " !");
+    		assertNotEquals(9.2D, actual);
+            assertNotNull(actual);
+    	}
+    }
+    
+    @Test
+    void testSquareRoot() {
+    	SimpleMath math6 = new SimpleMath(); {
+    		double number = 16D;
+    		Double actual = math6.squareRoot(number);
+    		double expected = 4D;
+    		
+    		assertEquals(expected, actual, () -> "Square Root of " + number + " did not produce " + expected + " !");
+    		assertNotEquals(9.2D, actual);
+            assertNotNull(actual);
+    	}
+    }
 }
