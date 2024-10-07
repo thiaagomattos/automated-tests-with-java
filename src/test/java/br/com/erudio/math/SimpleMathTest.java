486886409a00adcf885cd9e688cf19b3e00caf14
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-07 16:00:32 -0300

    added displayName annotation

--- src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -1,13 +1,19 @@
 package br.com.erudio.math;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathTest {
 
     @Test
-    void testSum() {
+    @DisplayName("Test 6.2 + 2 = 8.2")
+    void testSum_WhenSixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
@@ -21,11 +27,12 @@ class SimpleMathTest {
     
 
     @Test
+    @DisplayName("Test 6.2 - 2 = 4.2")
     void testSubtraction() {
-    	SimpleMath math2 = new SimpleMath(); {
+    	SimpleMath math = new SimpleMath(); {
     		double firstNumber = 6.2D;
             double secondNumber = 2D;
-            Double actual = math2.subtraction(firstNumber, secondNumber);
+            Double actual = math.subtraction(firstNumber, secondNumber);
             double expected = 4.2D;
             
             assertEquals(expected, actual, () -> firstNumber + "-" + secondNumber + " did not produce " + expected + "!");
@@ -35,12 +42,13 @@ class SimpleMathTest {
     }
     
     @Test
+    @DisplayName("Test 6.2 * 2 = 12.4")
     void testMultiplication() {
-    	SimpleMath math3 = new SimpleMath(); {
-    		double firstNumber = 5D;
+    	SimpleMath math = new SimpleMath(); {
+    		double firstNumber = 6.2;
     		double secondNumber = 2D;
-    		Double actual = math3.multiplication(firstNumber, secondNumber);
-    		double expected = 10D;
+    		Double actual = math.multiplication(firstNumber, secondNumber);
+    		double expected = 12.4D;
     		
     		assertEquals(expected, actual, () -> firstNumber + "*" + secondNumber + " did not produce " + expected + " !");
     		assertNotEquals(9.2D, actual);
@@ -49,26 +57,33 @@ class SimpleMathTest {
     }
     
     @Test
+    @DisplayName("Test 6.2 / 2 = 3.1")
     void testDivision() {
-    	SimpleMath math4 = new SimpleMath(); {
-    		double firstNumber = 10D;
+    	SimpleMath math = new SimpleMath(); {
+    		double firstNumber = 6.2;
     		double secondNumber = 2D;
-    		Double actual = math4.division(firstNumber, secondNumber);
-    		double expected = 5D;
+    		Double actual = math.division(firstNumber, secondNumber);
+    		double expected = 3.1D;
     		
-    		assertEquals(expected, actual, () -> firstNumber + ":" + secondNumber + " did not produce " + expected + " !");
+    		assertEquals(expected, actual, () -> firstNumber + "/" + secondNumber + " did not produce " + expected + " !");
     		assertNotEquals(9.2D, actual);
             assertNotNull(actual);
     	}
     }
+    	
+    @Test
+    void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException() {
+    		fail();
+    }
     
     @Test
+    @DisplayName("Test (6.2 + 2) / 2 = 4")
     void testMean() {
-    	SimpleMath math5 = new SimpleMath(); {
-    		double firstNumber = 10D;
+    	SimpleMath math = new SimpleMath(); {
+    		double firstNumber = 6.2;
     		double secondNumber = 2D;
-    		Double actual = math5.mean(firstNumber, secondNumber);
-    		double expected = 6D;
+    		Double actual = math.mean(firstNumber, secondNumber);
+    		double expected = 4.1D;
     		
     		assertEquals(expected, actual, () -> "(" + firstNumber + "+" + secondNumber + ")" + " /2 " + " did not produce " + expected + " !");
     		assertNotEquals(9.2D, actual);
@@ -78,9 +93,9 @@ class SimpleMathTest {
     
     @Test
     void testSquareRoot() {
-    	SimpleMath math6 = new SimpleMath(); {
+    	SimpleMath math = new SimpleMath(); {
     		double number = 16D;
-    		Double actual = math6.squareRoot(number);
+    		Double actual = math.squareRoot(number);
     		double expected = 4D;
     		
     		assertEquals(expected, actual, () -> "Square Root of " + number + " did not produce " + expected + " !");
