2ae094d8cbf50ecd982b00ee089605f7f23da401
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-07 17:46:45 -0300

    updated arrays test

--- src/test/java/br/com/erudio/ArraysCompareTest.java
+++ src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -3,8 +3,10 @@ package br.com.erudio;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
@@ -18,6 +20,20 @@ class ArraysCompareTest {
         assertArrayEquals(numbers, expectedArray);
     }
 
+    
+    @Test
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+    	
+        int[] numbers = {25,8,21,32,3};
+        for (int i = 0; i < 100000; i++) {
+        	numbers[0] = i;
+            Arrays.sort(numbers);
+
+        }
+        
+        
+    }
 }
 
 
