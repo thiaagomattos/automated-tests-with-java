98a7d325c190e86c159e01532f4e8694f7a8adb2
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-08 13:43:59 -0300

    ordered classes

--- src/test/java/br/com/erudio/MethodOrderedByOrder.java
+++ src/test/java/br/com/erudio/MethodOrderedByOrder.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@Order(3)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MethodOrderedByOrder {
     
